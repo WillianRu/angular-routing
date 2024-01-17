369a18895093d958412a15634b46eb523bdfad65
Willian <correo.willianrueda@gmail.com>
2024-01-17 11:37:12 -0500

    Routing process on Angular framework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'AboutUs',
+    component: AboutUsComponent
+  },
+  {
+    path: 'Contact',
+    component: ContactComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
